950b16099d6129745b20a6e31ad1d005d27bb17d
LBH <68373877+kr2020lbh@users.noreply.github.com>
2022-05-16 23:24:05 +0900

    Time: 53 ms (34.46%), Space: 13.9 MB (74.09%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.py
@@ -0,0 +1,10 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n = len(haystack)
+        m = len(needle)
+        if m == 0:
+            return 0
+        try: 
+            return haystack.index(needle)
+        except:
+            return -1
