bdd593f9b40211050d8ec752223bbc98d9a17231
LBH <68373877+kr2020lbh@users.noreply.github.com>
2022-05-22 22:37:32 +0900

    Time: 68 ms (5.13%), Space: 14 MB (30.11%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,35 @@
 from itertools import combinations
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        return self.solution2(digits)
+        
+        
+    def solution2(self,digits):
+        numString = [
+            [],
+            [],
+            ["a","b","c"],
+            ["d","e","f"],
+            ["g","h","i"],
+            ["j","k","l"],
+            ["m","n","o"],
+            ["p","q","r","s"],
+            ["t","u","v"],
+            ["w","x","y","z"]
+        ]
+        digits = list(map(int,digits[::-1]))
+        if not digits:
+            return []
+        tmp = numString[digits.pop()][::]
+        while digits:
+            ttmp = []
+            for letter in numString[digits.pop()]:
+                for t in tmp:
+                    ttmp.append(t + letter)
+            tmp = ttmp
+        return tmp
+        
+    def solution1(self,digits):
         numString = [
             [],
             [],
@@ -19,7 +48,6 @@ class Solution:
         if depthLimit:
             self.dfs(result,0,depthLimit,numString,digitsIntType,[])
         return result
-    
     
     def dfs(self,result,depth,depthLimit,numString,digits,indexes):
         if depth == depthLimit:
