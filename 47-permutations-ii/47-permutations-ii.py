a77094ea7e88da17ae504aba38f750b20b829059
LBH <68373877+kr2020lbh@users.noreply.github.com>
2022-06-23 22:27:55 +0900

    Time: 835 ms (10.04%), Space: 14.2 MB (75.97%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,15 +1,18 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ret = set()
-        self.func(nums,0,[],ret)
-        return [list(r) for r in ret]
+        tmpRet = set()
+        ret = []
+        self.func(nums,0,[],tmpRet,ret)
+        return ret
     
-    def func(self,nums,bit,tmp,ret):
+    def func(self,nums,bit,tmp,tmpRet,ret):
         if bit == 2**len(nums)-1:
-            ret.add(tuple(tmp))
+            if not (tuple(tmp) in tmpRet):
+                tmpRet.add(tuple(tmp))
+                ret.append(tmp)
             return
         for i in range(len(nums)):
             if not((1 << i) & bit):
-                self.func(nums,bit | (1<<i),tmp + [nums[i]], ret)
+                self.func(nums,bit | (1<<i),tmp + [nums[i]], tmpRet, ret)
                 
